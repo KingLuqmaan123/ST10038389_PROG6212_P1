8c9e1c1b243e226a7aa3ef26ce7f65d714d2e4ae
Luqmaan Aziz - ST10038389 <129889786+Astronuts1@users.noreply.github.com>
2024-09-10 12:57:50 +0200

    Add project files.

---
+++ ST10038389_Contract Monthly Claim System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10038389_Contract_Monthly_Claim_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
